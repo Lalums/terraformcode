bea7122626a93479b6e2adfac4fe28624e7056b1
ratheeshkumarms <ratheesh.kumarms@xe04.ey.com>
2020-06-04 16:19:32 +0530

    09

--- subnets.tf
+++ subnets.tf
@@ -7,23 +7,7 @@
   virtual_network_name = "${azurerm_virtual_network.myvnet.name}"
   #network_security_group_id = "${azurerm_network_security_group.MYManagement-nsg.id}"
   }
-  resource "azurerm_subnet" "mysubnet2" {
-  depends_on = ["azurerm_resource_group.myresourcegroup","azurerm_virtual_network.myvnet"]
-  name = "agw"
-  address_prefix = "10.1.2.0/24"
-  resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
-  virtual_network_name = "${azurerm_virtual_network.myvnet.name}"
-  #network_security_group_id = "${azurerm_network_security_group.MYAPPGW-nsg.id}"
-  }
- 
-  resource "azurerm_subnet" "mysubnet4" {
-  depends_on = ["azurerm_resource_group.myresourcegroup","azurerm_virtual_network.myvnet"]
-  name = "apim"
-  address_prefix = "10.1.4.0/24"
-  resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
-  virtual_network_name = "${azurerm_virtual_network.myvnet.name}"
-  #network_security_group_id = "${azurerm_network_security_group.MYapim-nsg.id}"
-  }  
+
   resource "azurerm_subnet" "mysubnet5" {
   depends_on = ["azurerm_resource_group.myresourcegroup","azurerm_virtual_network.myvnet"]
   name = "corda"
@@ -31,5 +15,5 @@
   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
   virtual_network_name = "${azurerm_virtual_network.myvnet.name}"
   #network_security_group_id = "${azurerm_network_security_group.MYCorda-nsg.id}"
-  service_endpoints    = ["Microsoft.Sql"]
+  #service_endpoints    = ["Microsoft.Sql"]
   }
