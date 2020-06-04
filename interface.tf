61d050d16865b6af4122491fd53e7e75e45d2517
ratheeshkumarms <ratheesh.kumarms@xe04.ey.com>
2020-06-04 18:36:10 +0530

    asas

--- interface.tf
+++ interface.tf
@@ -5,7 +5,7 @@ resource "azurerm_network_interface" "mynics" {
   name                      = "cordanic${count.index+1}"
   location                  = "${var.location}"
   resource_group_name       = "${azurerm_resource_group.myresourcegroup.name}"
-  depends_on = ["azurerm_resource_group.myresourcegroup","azurerm_virtual_network.myvnet","azurerm_subnet.mysubnet5","azurerm_public_ip.mypips"]
+  #depends_on = ["azurerm_resource_group.myresourcegroup","azurerm_virtual_network.myvnet","azurerm_subnet.mysubnet5"]
 
   ip_configuration {
     name                          = "ipconfig${count.index}"
