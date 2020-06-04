039c109cd0414152254311877625f45a11c4b566
ratheeshkumarms <ratheesh.kumarms@xe04.ey.com>
2020-06-04 15:53:55 +0530

    56

--- subnets.tf
+++ subnets.tf
@@ -1,5 +1,6 @@
 #Create Subnets
   resource "azurerm_subnet" "mysubnet1" {
+  depends_on = ["azurerm_resource_group.myresourcegroup","azurerm_virtual_network.myvnet"]
   name = "mgmt"
   address_prefix = "10.1.1.0/24"
   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
@@ -7,6 +8,7 @@
   #network_security_group_id = "${azurerm_network_security_group.MYManagement-nsg.id}"
   }
   resource "azurerm_subnet" "mysubnet2" {
+  depends_on = ["azurerm_resource_group.myresourcegroup","azurerm_virtual_network.myvnet"]
   name = "agw"
   address_prefix = "10.1.2.0/24"
   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
@@ -15,6 +17,7 @@
   }
  
   resource "azurerm_subnet" "mysubnet4" {
+  depends_on = ["azurerm_resource_group.myresourcegroup","azurerm_virtual_network.myvnet"]
   name = "apim"
   address_prefix = "10.1.4.0/24"
   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
@@ -22,6 +25,7 @@
   #network_security_group_id = "${azurerm_network_security_group.MYapim-nsg.id}"
   }  
   resource "azurerm_subnet" "mysubnet5" {
+  depends_on = ["azurerm_resource_group.myresourcegroup","azurerm_virtual_network.myvnet"]
   name = "corda"
   address_prefix = "10.1.5.0/24"
   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
