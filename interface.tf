d5a694de3fe8896b3d6d3935974198b22409a335
ratheeshkumarms <ratheesh.kumarms@xe04.ey.com>
2020-06-04 15:39:11 +0530

    3

--- interface.tf
+++ interface.tf
@@ -2,6 +2,7 @@
 
 resource "azurerm_network_interface" "mynics" {
   count = 1
+  depends_on = ["azurerm_resource_group.myresourcegroup","azurerm_virtual_network.myvnet"]
   name                      = "cordanic${count.index+1}"
   location                  = "${var.location}"
   resource_group_name       = "${azurerm_resource_group.myresourcegroup.name}"
