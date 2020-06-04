bee2956e644a9fa75c78c5a4a75cc751adddf963
ratheeshkumarms <ratheesh.kumarms@xe04.ey.com>
2020-06-04 14:56:17 +0530

    1

---
+++ provider.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+  default = "9ef813b3-40a3-416f-84c1-bc32a23fcc6d"
+}
+
+variable "client_id" {
+  default = "4cdef086-16b1-4ea4-b514-b26e2f69346b"
+}
+
+variable "client_secret" {
+  default = "9f436e18-f63b-48fe-b03b-8dc232a5da1e"
+}
+
+variable "tenant_id" {
+  default = "4c589a6b-55f9-4ce3-9989-8905609b0747"
+}
